8ed963f5371d283d3f457971821b6c3f8e998ca7
Gerardo Sabetta <sabettagerardo@gmail.com>
2020-10-27 21:08:25 -0300

    delete files from 48

--- encuentro-48/db.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize("mysql://root@192.168.64.2:3306/resto");
-
-const db = {
-  sequelize: sequelize,
-  Sequelize: Sequelize,
-};
-
-module.exports = db;
